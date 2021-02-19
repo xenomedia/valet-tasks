53da48ec5855195c8028f3248c74754aae6448a2
Albert Jankowski <albert@xenomedia.com>
2021-02-19 09:02:18 -0600

    Overloads the env data.

--- lib/valet-tasks/models/env_file.rb
+++ lib/valet-tasks/models/env_file.rb
@@ -5,6 +5,7 @@ module ValetTasks
         file = File.read(file_name)
         updated_line = file.gsub(/^.*#{Regexp.quote("#{variable}=")}.*$/, "#{variable}=#{value}")
         File.open(file_name, 'w') { |line| line.puts updated_line }
+        Dotenv.overload(file_name)
       end
     end
   end
