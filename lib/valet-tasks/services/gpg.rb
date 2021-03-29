efbb8c38fccc49b0f8845596a1bf240ca4d37c07
Albert Jankowski <albert@xenomedia.com>
2021-03-29 09:57:09 -0500

    Fixes typo.

--- lib/valet-tasks/services/gpg.rb
+++ lib/valet-tasks/services/gpg.rb
@@ -4,7 +4,7 @@ module ValetTasks
       def self.set_key_if_needed(env_file)
         key = 'GPG_KEY'
         if ENV.key?(key)
-          puts "What is the GPG key (This will be used for unarching the database backups in gpg format)?"
+          puts "What is the GPG key (This will be used for unarchiving the database backups in gpg format)?"
           STDOUT.flush
           value = STDIN.gets.chomp
           ValetTasks::Model::EnvFile.update_variable(env_file, key, value) unless value == ''
